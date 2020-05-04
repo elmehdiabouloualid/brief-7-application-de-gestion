cce62b57a44863c8526df2268f41840db63655ec
elmehdi abouloualid <57229450+elmehdiabouloualid@users.noreply.github.com>
2020-05-04 00:38:51 +0000

    brief user

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
+const app = express();
+
+
+app.set('view engine' , 'ejs');
+app.set('views', path.join(__dirname, 'views')); //default 'views'
+ 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
+app.use(require('./routes/index'));
+
+
+
+
+module.exports = app;
+
+
+app.listen(3000, () => console.log('listening on port ...'));
+//"http://localhost:3000"
